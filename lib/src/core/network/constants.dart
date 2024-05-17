e9f70b4dc557d71195ce134f27c6fdc5688d8ec0
Enver Erdem <envererdem18@gmail.com>
2024-05-17 18:52:20 +0300

    feat: Update API key constant to use placeholder

--- lib/src/core/network/constants.dart
+++ lib/src/core/network/constants.dart
@@ -1,4 +1,4 @@
 // TODO: obfuscate your API key using the guide:
 // https://codewithandrea.com/articles/flutter-api-keys-dart-define-env-files/
-const openApiKey = 'sk-proj-vA0YdP45F5LSGBJt7Bf0T3BlbkFJcTrkHpsvogzVKrx0Qo3W';
+const openApiKey = 'OPENAI_API_KEY';
 const baseUrl = 'https://api.openai.com/v1/';
