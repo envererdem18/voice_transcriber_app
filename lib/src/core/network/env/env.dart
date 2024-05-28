8a647cbee8863a31afaa07ca040a4c8f871fec55
Enver Erdem <envererdem18@gmail.com>
2024-05-28 15:35:03 +0300

    chore: Obfuscate API key constant in network constants

--- lib/src/core/network/env/env.dart
+++ lib/src/core/network/env/env.dart
@@ -2,6 +2,9 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
+// API key is obfuscated using this guide:
+// https://codewithandrea.com/articles/flutter-api-keys-dart-define-env-files/
+
 @Envied(path: '.env')
 final class Env {
   @EnviedField(varName: 'openAiKey', obfuscate: true)
